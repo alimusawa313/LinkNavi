a60878fdf8eb517158b3e47fbdd338d2b3261f26
Ali Musawa <musawahaidar123@gmail.com>
2024-04-03 01:16:24 +0700

    Initial Commit

---
+++ MiniChallenge1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniChallenge1
+//
+//  Created by Ali Musawa on 03/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniChallenge1/MiniChallenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge1App.swift
+//  MiniChallenge1
+//
+//  Created by Ali Musawa on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
